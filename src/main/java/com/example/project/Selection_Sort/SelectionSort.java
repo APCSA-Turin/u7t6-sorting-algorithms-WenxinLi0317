68c36cf8ab97544e4fb85a8ae54a9efb7dc8e326
WenxinLi0317 <wenxinl8@nycstudents.net>
2025-02-24 14:14:18 +0000

    dd

--- src/main/java/com/example/project/Selection_Sort/SelectionSort.java
+++ src/main/java/com/example/project/Selection_Sort/SelectionSort.java
@@ -8,24 +8,63 @@ public class SelectionSort {
 
     // PART A. implementing selection sort
     public static int[] selectionSort(int[] elements) {
+        for (int i =0; i < elements.length-1; i ++)
+        {
+            for (int j = 0; j < elements.length-1-i; j++)
+            {
+                int num1 = elements[j];
+                int num2 = elements[j+1];
+                if(num2 < num1 )
+                {
+                    int temp = num1;
+                    elements[j]=num2;
+                    elements[j+1]=temp;
+                }
+            }
+        }
+        /*
         for (int j = 0; j < elements.length - 1; j++) {
             int minIndex = j;
             for (int k = j + 1; k < elements.length; k++) {
+                //1,3,2,
+                //j =0, k =1
+                //min = 0
+                //if element[1]=3 <element[1] 1
+                //
                 if (elements[k] < elements[minIndex]) {
                     minIndex = k;
                 }
             }
+            //temp = element[j], element[0], 1
+            //elemeny[0]=element[0] = 1, 
             int temp = elements[j];
             elements[j] = elements[minIndex];
             elements[minIndex] = temp;
         }
-
+        */
         return elements;
     }
 
 
     // PART B. sorting a 1000-word list
     public static ArrayList<String> selectionSortWordList(ArrayList<String> words) {
+        for (int i =0; i < words.size()-1; i ++)
+        {
+            for (int j = 0; j < words.size()-1-i; j++)
+            {
+                String str1 = words.get(j);
+                String str2= words.get(j+1);
+                int compare = str1.compareTo(str2);
+
+              
+                if(compare>0)
+                {
+                    words.set(j, str2);
+                    words.set(j+1, str1);
+                }
+            }
+        }
+        /*
         for (int j = 0; j < words.size() - 1; j++) {
             int minIndex = j;
             for (int k = j + 1; k < words.size(); k++) {
@@ -37,6 +76,7 @@ public class SelectionSort {
             words.set(j, words.get(minIndex));
             words.set(minIndex, temp);
         }
+        */
         return words;
     }
 
